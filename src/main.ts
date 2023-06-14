357d9cd47ed0943452d6148f7cc56459ca1c3f17
camelopard187 <camelopard187@gmail.com>
2023-06-14 14:15:15 +0300

    feat: Set up a Swagger API documentation ✨

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
 import { VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 import { AppModule } from '~/app.module'
+import { version } from '~/../package.json'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableVersioning({ type: VersioningType.URI }).enableCors()
+
+  const config = new DocumentBuilder()
+    .setTitle('Doghouse API')
+    .setDescription(
+      `This API provides endpoints for managing a doghouse application.
+      It allows users to perform various operations related to doghouses`
+    )
+    .setVersion(version)
+    .addTag('doghouse')
+    .addTag('ping')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('api', app, document)
 
   await app.listen(3000)
 }
